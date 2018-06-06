39183af7e23b7c8cce0df917d681043cceb055a3
Sergio <Sergio@Sergio-PC>
2018-06-05 23:20:18 -0300

    agregado de paquetes

---
+++ src/java/aplicacion/controlador/beans/forms/LoginFormBean.java
@@ -0,0 +1,26 @@
+package aplicacion.controlador.beans.forms;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Sergio Romero
+ */
+@ManagedBean
+@ViewScoped
+public class LoginFormBean {
+
+    /**
+     * Creates a new instance of LoginFormBean
+     */
+    public LoginFormBean() {
+    }
+    
+}
